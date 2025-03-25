97df9b29ab7ec92c5095de25f072b3dfe10b1ff2
pekarskiy-a <alepekar@gmail.com>
2025-03-25 10:52:54 +0300

    base for app

---
+++ src/main/java/ru/sevbereg/loyaltyprogra/LoyaltyProgramApplication.java
@@ -0,0 +1,13 @@
+package ru.sevbereg.loyaltyprogra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyaltyProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sevbereg/loyaltyprogra/LoyaltyProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sevbereg.loyaltyprogra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
